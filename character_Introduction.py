9ab228d9745a612c1d6315af2916f57e2d8d9ffc
o0OoeveoO0o <evans.hidalgo@ucas-edu.net>
2026-08-20 18:26:51 +0000

    larose

---
+++ character_Introduction.py
@@ -0,0 +1,13 @@
+#Evans Hidalgo Character Introduction M4x
+
+name = input("Please insert your name: ")
+
+age = input("Please insert your age: ")
+
+job = input("Please insert your job: ")
+
+place = input("Finally, Where do you live? ")
+
+print("So your name is " + name +" My name is oO0oeveo0Oo, I'm -random number- years old, so you are " + age + " old?,I'm a -random job that also exists- and you are a " + job + " That's also cool but, I live in -some place that maybe exists- and you live in "+ place)
+
+#larose
