bb9c2a3526d5373a905bd594108f2f9a81a368cd
o0OoeveoO0o <evans.hidalgo@ucas-edu.com>
2026-08-20 09:53:06 -0600

    CH Intro completed

---
+++ intro.py
@@ -0,0 +1,11 @@
+#Evans Hidalgo character intro M4x
+
+name = input("Hello starnger please insert your name: ")
+
+age = input("Oh, Please insert your age now: ")
+
+job = input("Now insert  your job: ")
+
+place = input("finally where are you from? " )
+
+print("Hello " + name + " my name is oO0oeveo0Oo and I am -some number- years old, since you are " + age + " you can access this code, Im a person that exist so you are a " + job + " that's cool, finally I live in -some place that probably exists, and you live in " + place + "That's so cool" )
