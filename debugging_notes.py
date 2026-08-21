63535bb9c0e0f7c6d28b458c27610a8b2459fdf5
o0OoeveoO0o <evans.hidalgo@ucas-edu.com>
2026-08-21 09:51:57 -0600

    debugging notes

---
+++ debugging_notes.py
@@ -0,0 +1,29 @@
+#Evans Hidalgo Debugging notes
+
+print("hello)
+      
+#identification error
+
+
+
+
+people = 10
+print(poeple)"""
+
+#logic errors
+#fix them by reading code
+apples=20
+
+people = 3
+print(apples * people)
+
+while True:
+
+try:
+
+    fav_num = int(input("what is your favorite number"))
+    except:
+    print("That's not a number")
+    else:
+    break
+    print (4 + fav_num)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+while True:
+
+try:
+
+    fav_num = int(input("what is your favorite number"))
+    except:
+    print("That's not a number")
+    else:
+    break
+    print (4 + fav_num)
