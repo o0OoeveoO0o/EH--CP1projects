713b4b83ed95b3e0ec5445c4caef3e3a127913e5
o0OoeveoO0o <evans.hidalgo@ucas-edu.com>
2026-08-21 09:18:25 -0600

    Fix the code Assignment complete

---
+++ debugging.py
@@ -0,0 +1,7 @@
+#Evans Hidalgo Fix the code M4x
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+#There was a missing parentesis and name was not writed correctly
