82e084f8a529d92ac60b064d4d17e2c581242c18
o0OoeveoO0o <evans.hidalgo@ucas-edu.com>
2026-08-26 09:47:04 -0600

    completed

---
+++ unit1_final_project.py
@@ -0,0 +1,14 @@
+#Evans Hidalgo Unit one final project M4x
+
+#these are questions
+name = input ("What is your name? ")
+age = input ("How old are you? ")
+job = input ("What is your job? ")
+activities = input ("What do you like to do in your free time? ")
+food = input ("What is your favorite food? ")
+place = input ("Where do you live? ")
+animals = input ("Which animal from the world you like the most? ")
+hobby = input ("What is your hobby? ")
+
+#what is being printed from all the questions
+print("Hello my name is " + name + " I am " + age + " I am a/an " + job + " and I like to " + activities +  " in my free time, my favorite food is " +  food + " , I live in " + place + " I really like " + animals + " and my hobby is " + hobby)
