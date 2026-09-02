6fab5ee070ae34b858edd3cb9f3feee6ddb06d08
o0OoeveoO0o <evans.hidalgo@ucas-edu.net>
2026-09-02 09:50:24 -0600

    Notes

---
+++ Notes/lessons/strings.py
@@ -0,0 +1,27 @@
+#evans hidalgo
+name = "Ms. Larose"
+age= "15"
+print(age + '2')
+print(name+" "+age)
+first_name = "Vienna"
+last_name= "Larose"
+full_name = first_name + " " + last_name
+print(full_name)
+#escape char \
+#\n creates a new line
+#\t
+sentence = '\tThen he said \n"That isn\'t fair"'
+print (sentence)
+
+print("*" * 30)
+
+sentence=("The quick brown fox jumps over the lazy dog")
+print(sentence)
+print(sentence.find("s")) #detects the letter and where it is
+#strings are lists
+#index stars with 0
+print(sentence[20:25])
+word = input("what word do you want ?")
+start = sentence.find(word)
+lenght = len(word)
+
