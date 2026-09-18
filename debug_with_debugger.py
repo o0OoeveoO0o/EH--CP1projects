9d2ceef957fc20b9228ae9c4719c3663058681e6
o0OoeveoO0o <evans.hidalgo@ucas-edu.net>
2026-09-17 22:49:47 -0500

    assignment done

--- debug_with_debugger.py
+++ debug_with_debugger.py
@@ -1,22 +1,18 @@
 # Ravager Snack Bar
 import random
-
 pirate_name = input("What's your name, pirate? ")
 snack_name = input("What snack do you want? ")
-
 price = random.randint(2, 8)  # random price in credits
-quantity = input("How many would you like? ")
-
+#1 transform the variable below by adding an INT
+quantity = int(input("How many would you like? "))
 total = price * quantity
-
-discounted_total = total - 2 * 0.
-                        #^ Wrong sign
-
+#2 the old one was total - 2 * 0.10 but  it was just subtracts 2 now we have a 10% discount
+discounted_total = total - total * 0.10
 tax_rate = 0.08
 total_with_tax = discounted_total + (discounted_total * tax_rate)
-
-print("Hello, " + pirate_name + "! Here's your order summary:", total_with_tax ) #<-- ADD THE TOTAL TO THE PRINT STATEMENT
-print("Snack: " + snack_name) #<--Wrong name for the variable
+print("Hello, " + pirate_name + "! Here's your order summary:")
+print("Snack: " + snack_name) #3 <- Name error
 print("Price per snack: " + str(price) + " credits")
-print("Total before tax: " + str(price))
-print("Total with tax: " + str(round(total_with_tax, 2)) + " credits") #<- missing parentesis
+#4 now it prints the real correct variable
+print("Total before tax: " + str(discounted_total))
+print("Total with tax: " + str(round(total_with_tax, 2)) + " credits")# 5<- missing parentesis
