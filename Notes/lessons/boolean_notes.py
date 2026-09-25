665b93122cf3ec8148ac2615b38ff2d8291b8aa3
evans.hidalgo@ucas-edu.net <evans.hidalgo@ucas-edu.net>
2026-09-25 09:06:19 -0600

    notes and changes

---
+++ Notes/lessons/boolean_notes.py
@@ -0,0 +1,12 @@
+#Evans Hidalgo Boolean notes
+
+win = False
+
+admin = True #<- a real boolean have a capital letter 
+age = 18
+
+
+if 18 > age: #<-boolean equation because it will always result in a true or false. ************Comparison operators | < less than | > greater than | <= less than or equal to | == equal to | != not equal to | >= greater than or equal to |
+    print("Your are an adult")#<if the statement from the top is true this will happen 
+#i can convert anything to a boolean
+print(bool(age))# everything is true unless is empty (0 or " ")
